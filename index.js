c95858a0629bb89f571669df020d069e71f1da04
rifataktchy <rifataktchy@gmail.com>
2024-12-08 03:00:24 +0100

    nine commit

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello from crowd cube server!');
 });
 
 app.listen(port, () => {
